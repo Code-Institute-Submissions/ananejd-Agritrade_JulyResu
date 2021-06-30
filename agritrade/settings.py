b9634f438735ba70d2aa42c7f26672c418baf052
ananejd <ananejd@gmail.com>
2021-06-30 19:35:20 +0000

    Secret key removed and debug set

--- agritrade/settings.py
+++ agritrade/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i+jjnhb@6o+8-6@=(%6(2=mvona&9k-lk$auhy$d&fs4)q2n^a'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jd-agritrade.herokuapp.com', 'localhost']
 
